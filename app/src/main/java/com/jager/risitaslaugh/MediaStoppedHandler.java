7e166ea005792abc99799c487037899d45f12dd1
Jäger <david3355@gmail.com>
2016-05-25 17:19:13 +0200

    New Icons, package name change

---
+++ app/src/main/java/com/jager/risitaslaugh/MediaStoppedHandler.java
@@ -0,0 +1,9 @@
+package com.jager.risitaslaugh;
+
+/**
+ * Created by Jager on 2016.02.20..
+ */
+public interface MediaStoppedHandler
+{
+       void mediaStopped(int widgetID);
+}
--- app/src/main/java/laugh/jager/risitaslaugh/MediaStoppedHandler.java
+++
@@ -1,9 +0,0 @@
-package laugh.jager.risitaslaugh;
-
-/**
- * Created by Jager on 2016.02.20..
- */
-public interface MediaStoppedHandler
-{
-       void mediaStopped(int widgetID);
-}
